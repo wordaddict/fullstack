da81b39156f342d9ba945c029e9aad4b20e1c677
Adeyinka Micheal <madeyinka6@gmail.com>
2018-04-30 20:57:59 +0100

    refactor

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ require("./services/passport.js");
 const app = express();
 const port = process.env.PORT || 3000;
 
+require("./routes/authRoutes")(app);
+
 app.listen(port, () => {
   console.log(`App is listening on port ${port}`);
 });
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,10 +1,12 @@
 const passport = require("passport");
 
-app.get(
-  "/auth/google",
-  passport.authenticate("google", {
-    scope: ["profile", "email"]
-  })
-);
+module.exports = app => {
+  app.get(
+    "/auth/google",
+    passport.authenticate("google", {
+      scope: ["profile", "email"]
+    })
+  );
 
-app.get("/auth/google/callback", passport.authenticate("google"));
+  app.get("/auth/google/callback", passport.authenticate("google"));
+};
